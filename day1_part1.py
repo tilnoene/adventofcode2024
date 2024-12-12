30551d6de29d38740bf9c23d886e6f6cd354d1a0
Victor Santos <mbvictorsantos@gmail.com>
2024-12-12 16:23:01 -0300

    fix: Change identation on day 1 problems

--- day1_part1.py
+++ day1_part1.py
@@ -2,10 +2,10 @@ a = []
 b = []
 
 for i in range(1000):
-  line = input().split('   ')
+    line = input().split('   ')
 
-  a.append(int(line[0]))
-  b.append(int(line[1]))
+    a.append(int(line[0]))
+    b.append(int(line[1]))
 
 a.sort()
 b.sort()
@@ -13,6 +13,6 @@ b.sort()
 total_sum = 0
 
 for i in range(len(a)):
-  total_sum += abs(a[i] - b[i])
+    total_sum += abs(a[i] - b[i])
 
 print(total_sum)
--- day1_part2.py
+++ day1_part2.py
@@ -2,16 +2,16 @@ a = []
 cnt = {}
 
 for i in range(1000):
-  line = input().split('   ')
+    line = input().split('   ')
 
-  a.append(int(line[0]))
+    a.append(int(line[0]))
   
-  b = int(line[1])
-  cnt[b] = cnt.get(b, 0) + 1
+    b = int(line[1])
+    cnt[b] = cnt.get(b, 0) + 1
 
 total_sum = 0
 
 for i in range(len(a)):
-  total_sum += a[i] * cnt.get(a[i], 0)
+    total_sum += a[i] * cnt.get(a[i], 0)
 
 print(total_sum)
